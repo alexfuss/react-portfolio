423f4bb683f14080e19b4edc3aa0ed71c92629ec
Alex Fuss <alexfuss2@gmail.com>
2020-10-14 18:51:50 -0400

    Added more styling to the nav

--- alexfuss-app/src/components/Nav.js
+++ alexfuss-app/src/components/Nav.js
@@ -6,13 +6,13 @@ export default class Menu extends Component {
     render() {
         return (
           <div>
-          <Navbar collapseOnSelect expand="lg" className="nav">
+          <Navbar collapseOnSelect expand="lg" className="nav navbar">
           <img src={process.env.PUBLIC_URL + 'Images/16996ff1-4730-4e2e-8147-4f3cbe28dbbe_200x200.png'} width={50} height={50} alt="AF custom logo" loading="lazy" className="nav-img" />
             <Navbar.Toggle />
             <Navbar.Collapse>
               <Nav className="mr-auto nav-item">
-                <Nav.Link className="nav-link" style={{color: 'white'}} href="https://alexfuss.github.io/portfolio/">Portfolio</Nav.Link>
-                <Nav.Link className="nav-link" style={{color: 'white'}} download href="../portfolio/Assets/Fuss_Alexander_Resume_2020.pdf">Resume</Nav.Link>
+                <Nav.Link className="nav-link nav-item" style={{color: 'white'}} href="https://alexfuss.github.io/portfolio/">Portfolio</Nav.Link>
+                <Nav.Link className="nav-link nav-item" style={{color: 'white'}} download href="../portfolio/Assets/Fuss_Alexander_Resume_2020.pdf">Resume</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
