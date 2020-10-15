986c56b449b898021f5bffed46604e79e2d249b9
Alex Fuss <alexfuss2@gmail.com>
2020-10-15 17:24:47 -0400

    Updates

---
+++ precache-manifest.7f73beff5237146e6a8558995692e383.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeea73136067a8fe5c445d33fbfdc999",
+    "url": "/alexfuss/react-portfolio/index.html"
+  },
+  {
+    "revision": "a757a1b2e002ca4e1df4",
+    "url": "/alexfuss/react-portfolio/static/css/2.c476735f.chunk.css"
+  },
+  {
+    "revision": "38899c12cb2312ae5015",
+    "url": "/alexfuss/react-portfolio/static/css/main.594e7476.chunk.css"
+  },
+  {
+    "revision": "a757a1b2e002ca4e1df4",
+    "url": "/alexfuss/react-portfolio/static/js/2.6484bcb1.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/alexfuss/react-portfolio/static/js/2.6484bcb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38899c12cb2312ae5015",
+    "url": "/alexfuss/react-portfolio/static/js/main.e0939253.chunk.js"
+  },
+  {
+    "revision": "d96bc265d2e422e5cad9",
+    "url": "/alexfuss/react-portfolio/static/js/runtime-main.6bd61374.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/alexfuss/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/alexfuss/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/alexfuss/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/alexfuss/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/alexfuss/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
