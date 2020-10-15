62010e56c9e2346ad0f9c40950b5bf37cfca297f
Alex Fuss <alexfuss2@gmail.com>
2020-10-15 17:26:36 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alexfuss/react-portfolio/precache-manifest.7f73beff5237146e6a8558995692e383.js"
+  "/alexfuss/react-portfolio/tree/main/alexfuss-app/precache-manifest.2aec5e93e2a8de3aa71167c4d7abf945.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alexfuss/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alexfuss/react-portfolio/tree/main/alexfuss-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
